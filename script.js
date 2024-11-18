045699ca9e62768c200d2d652d31a9f5ac190b1d
FinleyBehringer <46537598+FinleyBehringer@users.noreply.github.com>
2024-11-18 09:57:55 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById("contactModal");
+const openModalButton = document.getElementById("contactBtn");
+const closeModalButton = document.getElementById("closeModal");
+
+openModalButton.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+closeModalButton.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
